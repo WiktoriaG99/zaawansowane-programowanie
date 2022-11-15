9513602457f846d7b742b4b8ed02689b82556eb0
WiktoriaG99 <wiktoriagajda99@gmail.com>
2022-11-15 22:52:30 +0100

    Modularyzacja kodu + Standard układania kodu PEP 8

---
+++ Classes/Class.py
@@ -0,0 +1,8 @@
+from flask_restful import Resource
+
+class Movie(Resource):
+    def __init__(self, movieID, title, genres):
+        self.movieID = movieID
+        self.title = title
+        self.genres = genres
+
--- main.py
+++ main.py
@@ -2,27 +2,21 @@ import csv
 import json
 from flask import Flask
 from flask_restful import Resource, Api
+import Classes.Class as Class
 
 app = Flask(__name__)
 api = Api(app)
-
-
-class Movie(Resource):
-    def __init__(self, movieID, title, genres):
-        self.movieID = movieID
-        self.title = title
-        self.genres = genres
 
 
 @app.route('/', methods=['GET'])
 def movie_page():
     movie_list = []
 
-    with open('test.csv', newline='') as csv_file:
+    with open('movies.csv', newline='') as csv_file:
         reader = csv.reader(csv_file)
         next(reader, None)  # Skip header
         for movieID, title, genres in reader:
-            movie_list.append(Movie(movieID, title, genres))
+            movie_list.append(Class.Movie(movieID, title, genres))
 
     data_set = json.dumps([ob.__dict__ for ob in movie_list], indent=5)
 
